b10ab55bb6ae3749ab200f83a8355766e7da128f
kdundigalla25 <kdundigalla25@gmail.com>
2023-01-28 15:27:33 -0500

    made todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState<string[]>([]);
+  const [todoName, setTodoName] = useState("");
+  const [count, setCount] = useState(0);
+
+  const handleSubmit = () => {
+    if (todoName == "") return;
+    setTodos([...todos, todoName]);
+    setTodoName("");
+    setCount(count + 1);
+  };
 
   return (
     <div className="App">
@@ -15,20 +24,47 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>ToDo List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input value={todoName} onChange={(e) => setTodoName(e.target.value)} />
+      </form>
+      <div className="parent">
+        <button className="submitButton" onClick={(e) => handleSubmit()}>
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          className="clearButton"
+          onClick={() => {
+            setTodos([]);
+          }}
+        >
+          Clear
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="listItems">
+        {todos.map((todo, i) => (
+          <div className="item">
+            <p className="item">
+              {i + 1}. {todo}
+            </p>
+            <button
+              className="xButton"
+              onClick={() =>
+                setTodos(todos.filter((innerTodo) => innerTodo != todo))
+              }
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
